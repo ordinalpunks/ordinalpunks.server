3968966a406afffc20c7f50f3e9387629c312480
Gerald Bauer <gerald.bauer@gmail.com>
2023-05-03 14:49:42 +0200

    up

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ordinalpunks/ordinalpunks.server
+
+go 1.18
+
+require github.com/pixelartexchange/artbase.server v1.1.0
+
+require github.com/learnpixelart/pixelart.go v0.0.0-20221027145508-aa6320022498 // indirect
---
+++ ordpunkd.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	// "log"
+	// "os"
+	"net/http"
+
+
+	"github.com/pixelartexchange/artbase.server/artbase"
+	"github.com/pixelartexchange/artbase.server/serve"
+)
+
+
+
+
+
+func main() {
+
+  url := "https://github.com/ordinalpunks/ordinalpunks.server/raw/master/collections.csv"
+
+	collections := artbase.DownloadCollections( url )
+
+	serve := serve.NewRouter( collections )
+
+	// default addr to localhost:8080 for now
+	//    for windows include localhost to avoid firewall warning/popup
+	//       if binding to :8080 only  - why? why not?
+
+  addr := "localhost:8080"
+
+ 	http.ListenAndServe( addr, serve )
+
+	fmt.Println( "Bye!")
+}
+
